91e9f433b1ed00c6ffb1fed5c82b46af410b4277
Adam Gobran <adamgobes@gmail.com>
2022-04-22 16:42:20 -0400

    first commit

---
+++ api/svgToPngBuffer.ts
@@ -0,0 +1,34 @@
+import Chromium from "chrome-aws-lambda";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import nodeHtmlToImage from "node-html-to-image";
+
+export async function handler(
+  req: VercelRequest,
+  res: VercelResponse
+) {
+  if (req.method != "POST") {
+    res.status(405).send("Only POST requests allowed");
+    return;
+  }
+
+  try {
+    const { svg } = req.body;
+    const pngBuffer = (await nodeHtmlToImage({
+      html: `<html><body><img src="${svg}" width="100%" height="auto" /></body></html>`,
+      quality: 100,
+      type: "png",
+      puppeteerArgs: {
+        args: [...Chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: Chromium.defaultViewport,
+        executablePath: await Chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      },
+      encoding: "base64",
+    })) as string;
+
+    res.status(200).json({ pngBuffer });
+  } catch (e) {
+    res.status(404);
+  }
+}
