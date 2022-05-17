397269e45289f8d905e6e17917e63636193cb25d
Adam Gobran <adamgobes@gmail.com>
2022-05-17 16:07:57 -0400

    try

--- api/svgToPngBuffer.ts
+++ api/svgToPngBuffer.ts
@@ -14,7 +14,7 @@ export default async function (req: VercelRequest, res: VercelResponse) {
 
     const pngBuffer = (await nodeHtmlToImage({
       html: `<html><body><img src="${svg}" width="100%" height="auto" /></body></html>`,
-      quality: 100,
+      quality: 50,
       type: "png",
       puppeteerArgs: {
         args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
