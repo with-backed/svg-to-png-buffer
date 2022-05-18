53042ef8ea764913b9d497b2004940c1358114b4
Adam Gobran <adamgobes@gmail.com>
2022-05-18 10:15:51 -0400

    check

--- api/svgToPngBuffer.ts
+++ api/svgToPngBuffer.ts
@@ -3,6 +3,8 @@ import chrome from "chrome-aws-lambda";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import nodeHtmlToImage from "node-html-to-image";
 
+const UNI_V3_ADDRESS = "0xc36442b4a4522e871399cd717abdd847ab11fe88";
+
 export default async function (req: VercelRequest, res: VercelResponse) {
   if (req.method != "POST") {
     res.status(405).send("Only POST requests allowed");
@@ -10,7 +12,17 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   }
 
   try {
-    const { svg } = req.body;
+    const { svg, contractAddress } = req.body as {
+      svg: string;
+      contractAddress: string;
+    };
+
+    let html: string = "";
+    if (contractAddress.toLowerCase() === UNI_V3_ADDRESS) {
+      html = `<img src="${svg}" height="100%" width="auto" />`;
+    } else {
+      html = `<html><body><img src="${svg}" width="100%" height="auto" /></body></html>`;
+    }
 
     const pngBuffer = (await nodeHtmlToImage({
       html: `<img src="${svg}" height="100%" width="auto" />`,
